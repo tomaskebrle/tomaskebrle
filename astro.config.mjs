dccf7136409869846b17e0ad016d1525b1213664
Tomáš Kebrle <tech@tomaskebrle.cz>
2022-11-26 18:30:06 +0100

    Various changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import ogimage from "astro-og-image";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     svelte(),
+    mdx(),
     ogimage({
       config: {
         path: "/posts/",
