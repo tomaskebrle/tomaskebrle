d77b88054b74fa5ea9da91e2f7f095c2628f5a2e
Tomáš Kebrle <tech@tomaskebrle.cz>
2022-11-28 18:21:57 +0100

    :sparkles: Add load

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+      colors: {
+        primary: "#447DA6",
+        secondary: "#141414",
+        tertiary: "#595959"
+      }
+    },
+	},
+	plugins: [],
+}
